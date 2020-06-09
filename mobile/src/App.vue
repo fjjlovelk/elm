0f33d6ebb62021c50e2bde5be3c7277ea4cf6ddd
fjj <2730809743@qq.com>
2020-06-09 15:52:16 +0800

    初始化mobile

---
+++ mobile/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <van-tabbar>
+      <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="orders-o">订单</van-tabbar-item>
+      <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+    <router-view />
+  </div>
+</template>
+
+<style>
+</style>
