1bbfb6545a4cd8a5b9b9fbf08f305de4d5053895
fjj <2730809743@qq.com>
2020-09-14 06:40:39 +0800

    测试

--- server/routes/mobile/login.js
+++ server/routes/mobile/login.js
@@ -4,8 +4,10 @@ const jwt = require('jsonwebtoken')
 const User = require('../../models/User')
 
 router.post('/', async (req, res) => {
-  // try {
-    const { username, password } = req.body
+  try {
+    let { username, password } = req.body
+    username = String(username)
+    password = String(password)
     const user = await User.findOne({ username }).select('+password')
     let token = null
     if (!user) {
@@ -13,20 +15,22 @@ router.post('/', async (req, res) => {
       const data = await User.findOne({ username: newUser.username })
       token = jwt.sign({ id: newUser._id }, req.app.get('secret'))
       res.sendResult({ token, data }, 200, '登录成功')
+      res.sendResult(token, 200, '登录成功')
     } else {
       token = jwt.sign({ id: user._id }, req.app.get('secret'))
       const isValid = require('bcrypt').compareSync(password, user.password)
       if (isValid) {
         const data = await User.findOne({ username })
         res.sendResult({ token, data }, 200, '登录成功')
+        res.sendResult(token, 200, '登录成功')
       } else {
         res.sendResult(isValid, 422, '密码错误')
       }
     }
-  // } catch (error) {
-  //   res.sendResult(false, 500, '服务器内部错误')
-  //   throw error
-  // }
+  } catch (error) {
+    res.sendResult(error, 500, '服务器内部错误')
+    throw error
+  }
 
 })
 
