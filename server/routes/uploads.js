9953683d5b2471bf2f270e4c39a833a9b9972d34
fjj <2730809743@qq.com>
2020-09-14 04:38:50 +0800

    修改图片上传地址

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -4,7 +4,7 @@ const multer = require('multer')
 const upload = multer({ dest: __dirname + '/../uploads' })
 
 router.post('/', upload.single('file'), (req, res) => {
-  req.file.url = `http://localhost:3000/uploads/${req.file.filename}`
+  req.file.url = `http://www.fjj.cool/uploads/${req.file.filename}`
   res.send(req.file)
 })
 
