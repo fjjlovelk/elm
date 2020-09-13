fb296f64ac7cd99f7b34b07d5ccf897e0abeeac8
fjj <2730809743@qq.com>
2020-09-14 02:05:09 +0800

    将打包文件添加到server的public文件夹中

---
+++ server/public/admin/js/chunk-2d22cdfc.ca69233a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22cdfc"],{f4a9:function(e,n,c){"use strict";c.r(n);var t=function(){var e=this,n=e.$createElement,c=e._self._c||n;return c("div",[e._v(" homepage ")])},u=[],a={},l=a,o=c("2877"),r=Object(o["a"])(l,t,u,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d22cdfc.ca69233a.js.map
